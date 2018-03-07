024b54d5397de4f946462e3d60133799b8f1d6a2
Simon Mourier <simon_mourier@hotmail.com>
2018-03-07 08:13:29 +0100

    Added FA5 _variables.less file.

--- FontAwesomeEnum/Properties/AssemblyInfo.cs
+++ FontAwesomeEnum/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 #endif
 [assembly: AssemblyCompany("Simon Mourier")]
 [assembly: AssemblyProduct("FontAwesomeEnum")]
-[assembly: AssemblyCopyright("Copyright (C) 2013-2017 Simon Mourier. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright (C) 2013-2018 Simon Mourier. All rights reserved.")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("20afc51f-948b-4b2d-9d04-4e7784983075")]
